93d4ebe4f96601f64bc02bea60cef1d9c8235c5c
Liuyunda <man021436@163.com>
2021-01-12 23:58:19 +0800

    项目初始化

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,203 @@
+<template>
+  <div>
+    <el-tree :data="menus" :props="defaultProps" :expand-on-click-node="false" show-checkbox node-key="catId"
+             :default-expanded-keys="expandKey">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button
+          v-if="node.level <= 2"
+          type="text"
+          size="mini"
+          @click="append(data)">
+          Append
+        </el-button>
+        <el-button
+          type="text"
+          size="mini"
+          @click="edit(data)">
+          Edit
+        </el-button>
+        <el-button
+          v-if="node.childNodes.length==0"
+          type="text"
+          size="mini"
+          @click="remove(node, data)">
+          Delete
+        </el-button>
+      </span>
+      </span>
+    </el-tree>
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogVisible"
+      :close-on-click-modal="false"
+      width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input v-model="category.productUnit" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        title: '',
+        menus: [],
+        defaultProps: {
+          children: 'children',
+          label: 'name'
+        },
+        expandKey: [],
+        dialogVisible: false,
+        dialogType: '',
+        category: {
+          catId: null,
+          name: '',
+          parentCid: 0,
+          catLevel: 0,
+          showStatus: 1,
+          sort: 0,
+          productUnit: '',
+          icon: ''
+        }
+      }
+    },
+    methods: {
+      getMenus () {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get',
+          params: this.$http.adornParams()
+        }).then(({data}) => {
+          this.menus = data.data
+        })
+      },
+      append (data) {
+        this.dialogType = 'add'
+        this.title = '添加分类'
+        this.dialogVisible = true
+        this.category.name = ''
+        this.category.parentCid = data.catId
+        this.category.catLevel = data.catLevel * 1 + 1
+        this.category.catId = null
+        this.category.icon = ''
+        this.category.productUnit = ''
+        this.category.showStatus = 1
+        this.category.sort = 0
+      },
+      remove (node, data) {
+        var ids = [data.catId]
+        this.$confirm(`是否删除【${data.name}】菜单?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$http({
+            url: this.$http.adornUrl('/product/category/delete'),
+            method: 'post',
+            data: this.$http.adornData(ids, false)
+          }).then(({data}) => {
+            this.$message({
+              type: 'success',
+              message: '菜单删除成功!'
+            })
+            // 刷新菜单
+            this.getMenus()
+            // 设置默认展开的节点
+            this.expandKey = [node.parent.data.catId]
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+      },
+      addCategory () {
+        console.log('提交的数据', this.category)
+        this.$http({
+          url: this.$http.adornUrl('/product/category/save'),
+          method: 'post',
+          data: this.$http.adornData(this.category, false)
+        }).then(({data}) => {
+          this.$message({
+            type: 'success',
+            message: '菜单保存成功!'
+          })
+          // 关闭对话框
+          this.dialogVisible = false
+          this.getMenus()
+          this.expandKey = [this.category.parentCid]
+        })
+      },
+      editCategory () {
+        var {catId, name, icon, productUnit} = this.category
+        var data = {catId, name, icon, productUnit}
+        this.$http({
+          url: this.$http.adornUrl('/product/category/update'),
+          method: 'post',
+          data: this.$http.adornData(data, false)
+        }).then(({data}) => {
+          this.$message({
+            message: '菜单修改成功！',
+            type: 'success'
+          })
+          // 关闭对话框
+          this.dialogVisible = false
+          this.getMenus()
+          this.expandKey = [this.category.parentCid]
+        })
+      },
+      edit (data) {
+        this.dialogType = 'edit'
+        this.title = '修改分类'
+        this.dialogVisible = true
+        // 发送请求获取当前节点最新的数据
+        this.$http({
+          url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+          method: 'get'
+        }).then(({data}) => {
+          console.log(data)
+          this.category.name = data.data.name
+          this.category.catId = data.data.catId
+          this.category.icon = data.data.icon
+          this.category.productUnit = data.data.productUnit
+          this.category.parentCid = data.data.parentCid
+          this.category.catLevel = data.data.catLevel
+          this.category.showStatus = data.data.showStatus
+          this.category.sort = data.data.sort
+        })
+      },
+      submitData () {
+        if (this.dialogType === 'add') {
+          this.addCategory()
+        }
+        if (this.dialogType === 'edit') {
+          this.editCategory()
+        }
+      }
+    },
+    created () {
+      this.getMenus()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
