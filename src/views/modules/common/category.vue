072acf3c4aa63e3cdedd6cb9e7cc00f23e3bda9f
Liuyunda <man021436@163.com>
2021-01-26 23:42:22 +0800

    1.平台属性-属性分组：功能完善，实现分类树与属性关联显示

---
+++ src/views/modules/common/category.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-tree :data="menus" :props="defaultProps" node-key="catId" ref="menuTree" @node-click="nodeClick">
+  </el-tree>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        menus: [],
+        expandKey: [],
+        defaultProps: {
+          children: 'children',
+          label: 'name'
+        }
+      }
+    },
+    methods: {
+      getMenus () {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get',
+          params: this.$http.adornParams()
+        }).then(({data}) => {
+          this.menus = data.data
+        })
+      },
+      nodeClick (data, node, component) {
+        // 向父组件发送事件
+        this.$emit('tree-node-click', data, node, component)
+      }
+    },
+    created () {
+      this.getMenus()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
